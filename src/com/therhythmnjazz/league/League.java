099b401b217f5d5264c9b1efd32b94b2cc7c049a
Luke <luke@lukeparker.co.uk>
2013-07-17 17:57:57 +0100

    Added config.yml & Arenas.yml

--- src/com/therhythmnjazz/league/League.java
+++ src/com/therhythmnjazz/league/League.java
@@ -1,18 +1,52 @@
 package com.therhythmnjazz.league;
 
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 public class League extends JavaPlugin {
 
     private LeagueLobbyHandler lobbyHandler = new LeagueLobbyHandler();
 
+    public static File arenasConfigFile;
+
+    public static FileConfiguration arenasYML;
+
     public void onEnable() {
 
+        FileConfiguration config = getConfig();
+
+        // Add Default Config here
+
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+        saveDefaultConfig();
+
+        String pluginFolder = getDataFolder().getAbsolutePath();
+
+        arenasConfigFile = new File(pluginFolder + File.separator + "Arenas.yml");
+
+        if(!arenasConfigFile.exists()){
+            try{
+                arenasConfigFile.mkdir();
+            }catch(Exception Ex){
+                getLogger().severe("Unable to create Arenas config file!");
+            }try{
+                arenasYML.load(arenasConfigFile);
+            }catch(Exception Ex){
+                getLogger().severe("Unable to load Arenas.YML");
+            }
+        }
+
+        getLogger().info("Plugin enabled!");
     }
 
     public void onDisable() {
 
+
+        getLogger().info("Plugin disabled!");
     }
 
     public LeagueLobbyHandler getLobbyHandler() {
